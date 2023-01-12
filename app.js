f7892c499af91f9df16b4b4c50267d4e4853b45b
moditya <k.moditya@gmail.com>
2023-01-12 20:26:57 +0000

    commit

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // import Express and routes
-import express from 'express';
-import routes from './source/routes/route.js';
-
+import express from "express";
+import routes from "routes";
 // vary these constants according to where you are running (GCS, VMs, K8S) and how many VMs you have, if that's the lesson)
 // to do: this should be a command-line parameter that lets the app know how it is running
 //const arrNodes = [ "localhost" ]                                    // for testing on GCS
